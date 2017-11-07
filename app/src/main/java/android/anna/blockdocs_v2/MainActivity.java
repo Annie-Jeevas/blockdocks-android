d588bc10a9a422b7fbfad93f58a1c7fa33a3e3c6
Annie Jeevas <anna.light.terry@gmail.com>
2017-11-07 20:41:10 +0300

    web3j: async getVersion

--- app/src/main/java/android/anna/blockdocs_v2/MainActivity.java
+++ app/src/main/java/android/anna/blockdocs_v2/MainActivity.java
@@ -3,9 +3,9 @@ package android.anna.blockdocs_v2;
 import android.anna.blockdocs_v2.model.Doc;
 import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +13,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.sql.Date;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.Web3jFactory;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
 
 
@@ -25,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
     Logger log = Logger.getLogger("Main");
     ArrayList<Doc> docs = new ArrayList<>();
     private Context context;
-
 
     class DocArrayAdapter extends ArrayAdapter<Doc> {
         private Context context;
@@ -57,19 +62,6 @@ public class MainActivity extends AppCompatActivity {
             qualification.setText(doc.getQualification());
             graduationDate.setText(doc.getGraduationDate());
 
-//            //display trimmed excerpt for description
-//            int descriptionLength = property.getDescription().length();
-//            if(descriptionLength >= 100){
-//                String descriptionTrim = property.getDescription().substring(0, 100) + "...";
-//                description.setText(descriptionTrim);
-//            }else{
-//                description.setText(property.getDescription());
-//            }
-
-//            //get the image associated with this property
-//            int imageID = context.getResources().getIdentifier(property.getImage(), "drawable", context.getPackageName());
-//            image.setImageResource(imageID);
-
             return view;
         }
     }
@@ -79,15 +71,28 @@ public class MainActivity extends AppCompatActivity {
         log.info("In onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         docs.add(new Doc(1, 1716480277, "2016-12-12", "Bachelor", "Information systems and technologies"));
         docs.add(new Doc(2, 1720456993, "2020-12-14", "Master", "Information systems and technologies"));
+
         ArrayAdapter arrayAdapter = new DocArrayAdapter(this, 0, docs);
         ListView listView = (ListView) findViewById(R.id.docsListView);
         listView.setAdapter(arrayAdapter);
-        log.info(String.valueOf(docs.size()));
     }
 
     public void onBlockClick(View view) {
         log.info("In onClick for ConstraintLayout");
+
+        try {
+            Web3j web3 = Web3jFactory.build(new HttpService("http://13.81.213.199:8545"));  // defaults to http://localhost:8545/
+            Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().sendAsync().get();
+            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+            log.info("clientVersion " + clientVersion);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
     }
 }
